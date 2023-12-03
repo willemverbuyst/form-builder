b38fed4a1604b14c703fd33996495be310e11b15
Willem Verbuyst <willemverbuyst@gmail.com>
2023-12-03 15:50:38 +0100

    Add dummy print button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,7 @@
+import { IoPrintOutline, IoSaveOutline } from "react-icons/io5";
 import styled from "styled-components";
-import SaveBtn from "./buttons/saveBtn";
+import Button from "./wrappers/button";
+import Icon from "./wrappers/icon";
 
 const Wrapper = styled.section`
   padding: 0.5rem;
@@ -14,11 +16,33 @@ const HeaderText = styled.h1`
   color: var(--color-gold);
 `;
 
+const Buttons = styled.section`
+  display: flex;
+  gap: 0.5rem;
+`;
+
 export default function Header() {
+  const customBtnStyles = {
+    width: "4rem",
+    backgroundColor: "var(--color-gold)",
+    color: "var(--color-black)",
+  };
+
   return (
     <Wrapper>
       <HeaderText>Form Builder</HeaderText>
-      <SaveBtn />
+      <Buttons>
+        <Button customStyles={customBtnStyles}>
+          <Icon>
+            <IoPrintOutline />
+          </Icon>
+        </Button>
+        <Button customStyles={customBtnStyles}>
+          <Icon>
+            <IoSaveOutline />
+          </Icon>
+        </Button>
+      </Buttons>
     </Wrapper>
   );
 }
